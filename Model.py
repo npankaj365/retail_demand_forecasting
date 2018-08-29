21ce08baa78ce8dc8da660c71a239bf865698425
Pankaj Niroula <pankajniroula365@gmail.com>
2018-08-29 10:21:04 +0545

    Last working state before removing multiple loads

--- Model.py
+++ Model.py
@@ -6,13 +6,23 @@ from statsmodels.tsa.arima_model import ARIMA
 from sklearn import model_selection
 from sklearn.metrics import mean_squared_error, mean_absolute_error 
 
+np.warnings.filterwarnings('ignore')
+
 class Model():
+    name = ''
+
     def __init__(self, file):
+        print("Started {} Model".format(self.name))
         self.data = self.load_data(file)
+        print("Completed Loading")
         self.select_data()
+        print("Completed Selecting")
         self.intrapolate_data()
+        print("Completed Filling")
         self.split_data()
+        print("Completed Splitting")
         self.core_model()
+        print("Completed {} Model".format(self.name))
 
     def load_data(self, file):
         csv_data = pd.read_csv(file, index_col='Day', parse_dates=['Day', 'Fiscal Week'], date_parser=lambda s: dtparser.parse(s).date())
@@ -29,7 +39,9 @@ class Model():
         self.X = self.sales_data.reindex(date_range, fill_value=0).values
 
     def split_data(self):
-        self.X_train, self.X_test = model_selection.train_test_split(self.X, test_size = 0.2)
+        # self.X_train, self.X_test = model_selection.train_test_split(self.X, test_size = 0.2)
+        size = int(len(self.X) * 0.66)
+        self.X_train, self.X_test = self.X[0:size], self.X[size:len(self.X)]
         print("Training Set size: {} and Test Set size: {}".format(self.X_train.size, self.X_test.size))
         self.X_train = list(self.X_train)
 
@@ -84,66 +96,69 @@ class HoltWinter_Model(Model):
 
     def __init__(self, file):
         self.alpha, self.beta, self.gamma = 0.1, 0.1, 0.1
-        self.L = 7
+        self.season_length = 4
         super().__init__(file)
 
-    def initial_trend(self, X_train, L):
+    def initial_trend(self, X_train, season_length):
         total = 0
-        for i in range(L):
-            t = (X_train[i+L] - X_train[i])
-            t /= L
+        for i in range(season_length):
+            t = (X_train[i+season_length] - X_train[i])
+            t /= season_length
             total += t
-        return total / L
+        return total / season_length
 
-    def initial_season_components(self, X_train, L):
-        no_of_seasons = int(len(X_train)/L)
+    def initial_season_components(self, X_train, season_length):
+        no_of_seasons = int(len(X_train)/season_length)
         season_averages = []
         seasonal_data = {}
         
         # Find season averages
         for season in range(no_of_seasons):
-            i = L*season
-            season_data = X_train[i:i+L]
-            season_avg = sum(season_data)/L
+            i = season_length*season
+            season_data = X_train[i:i+season_length]
+            season_avg = sum(season_data)/season_length
             season_averages.append(season_avg)
             
         # Find initial values for each season
-        for i in range(L):
+        for i in range(season_length):
             total_over_avg = 0
             for season in range(no_of_seasons):
-                ind = L*season
+                ind = season_length*season
                 total_over_avg += X_train[ind+i] - season_averages[season]
             seasonal_data[i] = total_over_avg/no_of_seasons
         return seasonal_data
 
-    def triple_exponential_smoothing(self, X_train, X_test, L, alpha, beta, gamma, no_to_predict):
+    def triple_exponential_smoothing(self, X_train, X_test, season_length, alpha, beta, gamma, no_to_predict):
         preds = []
-        seasonal_data = self.initial_season_components(X_train, L)
+        seasonal_data = self.initial_season_components(X_train, season_length)
             
         smooth = X_train[0]
-        trend = self.initial_trend(X_train, L)
+        trend = self.initial_trend(X_train, season_length)
         result = [X_train[0]]
         
-        for i in range(L + no_to_predict):
+        for i in range(season_length + no_to_predict):
             if i >= len(X_train):
                 m = i - len(X_train) + 1
-                val = smooth + m * trend + seasonal_data[i % L]
+                val = smooth + m * trend + seasonal_data[i % season_length]
                 result.append(val)
             else:
                 val = X_train[i]
-                last_smooth, smooth = smooth, alpha * (val - seasonal_data [i%L]) + (1-alpha) * (smooth + trend)
+                last_smooth, smooth = smooth, alpha * (val - seasonal_data [i%season_length]) + (1-alpha) * (smooth + trend)
                 trend = beta * (smooth - last_smooth) + (1-beta) * trend
-                seasonal_data[i % L] = gamma * (val-smooth) + (1-gamma) * seasonal_data[i%L]
-                tot = smooth + trend + seasonal_data[i%L]
+                seasonal_data[i % season_length] = gamma * (val-smooth) + (1-gamma) * seasonal_data[i%season_length]
+                tot = smooth + trend + seasonal_data[i%season_length]
                 result.append(tot)
         return result
 
     def core_model(self):
         no_to_predict = len(self.X_train) + len(self.X_test)
-        result = self.triple_exponential_smoothing(self.X_train, self.X_test, self.L, self.alpha, self.beta, self.gamma, no_to_predict)
+        result = self.triple_exponential_smoothing(self.X_train, self.X_test, self.season_length, self.alpha, self.beta, self.gamma, no_to_predict)
 
-        self.mse = self.get_mse(self.X_train, result[:-len(self.X_test) - self.L - 1])
-        self.mae = self.get_mae(self.X_train, result[:-len(self.X_test) - self.L - 1])
+        self.mse = self.get_mse(self.X_train, result[:-len(self.X_test) - self.season_length - 1])
+        self.mae = self.get_mae(self.X_train, result[:-len(self.X_test) - self.season_length - 1])
+
+        # self.mse = self.get_mse(self.X_test, result[:-self.season_length - 1])
+        # self.mae = self.get_mae(self.X_test, result[:-self.season_length - 1])
         
         print("Mean Squared error = {:.4f}".format(self.mse))
         print("Mean Absolute error = {:.4f}".format(self.mae))
